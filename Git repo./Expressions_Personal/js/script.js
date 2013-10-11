eca8f0b0308230c8297fbdbe6fb403518ef8ea60
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-10 23:57:08 -0400

    Added expression diapers = kids * diaperChange and the console.log

--- Git repo./Expressions_Personal/js/script.js
+++ Git repo./Expressions_Personal/js/script.js
@@ -1,3 +1,5 @@
 //Alicia Zegelien 10/9/13 Expressions_Personal
 var kids = prompt("Number of kids in the room:");
 var diaperChange = prompt("Number of times the kids are changed daily");
+var diapers = kids * diaperChange;
+console.log(diapers);
