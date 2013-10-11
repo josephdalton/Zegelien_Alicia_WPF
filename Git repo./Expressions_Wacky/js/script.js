cf2120c3bb1a68e2605b7d905057ebfe3bf71297
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-11 14:38:53 -0400

    Added equation of arrays 0+1+2 = number of trick or treaters

--- Git repo./Expressions_Wacky/js/script.js
+++ Git repo./Expressions_Wacky/js/script.js
@@ -1,3 +1,4 @@
 //Alicia Zegelien 10-9-13 Expresson_Wacky
 var trickOrTreaters = [45,70,52];
-var totalOfTrickOrTreaters
+var totalOfTrickOrTreaters = trickOrTreaters[0] + trickOrTreaters[1] + trickOrTreaters[2];
+
