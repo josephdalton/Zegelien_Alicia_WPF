d85fb157053e1ef23a786577d16491a40fe20058
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-11 13:48:49 -0400

    Added the variable batteriesToChange, added mic * AA, and added the counsole.log to see if it worked. It worked.

--- Git repo./Expressions_Industy/js/script.js
+++ Git repo./Expressions_Industy/js/script.js
@@ -1,3 +1,5 @@
 //Alicia Zegelien 10-9-13 Expressions_Industry
 var mic = prompt("Number of mics");
-var AA = prompt ("Number of batteries in mic")
+var AA = prompt ("Number of batteries in mic");
+var micBatteriesToChange = mic * AA;
+console.log(micBatteriesToChange);
