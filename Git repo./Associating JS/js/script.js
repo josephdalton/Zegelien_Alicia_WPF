af33360e5a4649c7e7b55f4fcab8a7e4f80548a9
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-15 19:52:10 -0400

    Example of conditional logic with expressions in the code

--- Git repo./Associating JS/js/script.js
+++ Git repo./Associating JS/js/script.js
@@ -1,9 +1,11 @@
 //Conditdional logic
 
-var oldEnough = false;
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
-if(oldEnough){
+//if the kid is over 48 inches is height
+if(kidHeight + sneakerLift > minHeight){
 	console.log("You can ride the coaster!");
 }
-console.log("what comes after");
