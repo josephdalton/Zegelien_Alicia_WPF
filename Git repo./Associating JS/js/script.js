6213d07e019605da1775a83d93738cff337cb80f
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-15 19:42:55 -0400

    Example of Conditional logic code

--- Git repo./Associating JS/js/script.js
+++ Git repo./Associating JS/js/script.js
@@ -1,1 +1,9 @@
-alert("This is in my HTML file");
+//Conditdional logic
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+	console.log("You can ride the coaster!");
+}
+console.log("what comes after");
