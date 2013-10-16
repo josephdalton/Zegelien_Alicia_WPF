63dcbc15c0755df508f851a70f1fb9f3f5e3a7f0
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-15 20:58:12 -0400

    Example of using else if

--- Git repo./Associating JS/js/script.js
+++ Git repo./Associating JS/js/script.js
@@ -1,11 +1,15 @@
-//Conditdional logic
+//Conditdional logic - else if
 
-var kidHeight = 47;
+var kidHeight = 50;
 var minHeight = 48;
-var sneakerLift = 2;
+var wParentHeight =45;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches is height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
 	console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+	console.log("You can ride, but only with a parent present!");
+}else{
+	console.log("Sorry kid grow some first!");
 }
