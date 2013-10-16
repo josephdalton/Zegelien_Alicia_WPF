53f938b53b59da6e7f9e193e029be2e67a4e8a89
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-16 16:30:23 -0400

    This is the Stuff Your Face I Example.

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,18 @@
+// Alicia Zegelien 10/16/2013 Conditionals_worksheet
+
+// Example
+// Stuff your face I
+
+// Competitor's Weight?
+// must weigh 250 or more
+var competitorsWeight = 245;
+var minWeight = 250;
+if(competitorsWeight >= minWeight){
+	//if competitor is greater than or equall to 250
+	console.log("The competitor qualifies for the heavyweight division!");
+}else{
+	//if it is less than 250
+	console.log("The competitor needs to gain some weight!");
+}
+
+
