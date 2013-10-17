2ca97e7e466b67ce4230db0d63df532a699aa7b7
Alicia Zegelien <aliciazegelien@Alicias-MacBook-Pro.local>
2013-10-17 18:53:47 -0400

    Added comment on how I wany my if statement laid out. If >75 make 100 if not make 75(bulletins)

--- Zegelien_Alicia_Conditionals/Conditional_Personal/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Personal/js/script.js
@@ -1,4 +1,4 @@
 //Alicia Zegelien 10/17/2013 Conditional_Personal
 // The variable I am going to use for this assignment is people
 var people = prompt("Number of people per service?");
-// If num
+// If number of people is greater than 75 make 100 bulletins. Otherwise, make 75.
