3e9736bcf8ab9e4d1b5683bb2dc0c63e25aa0e07
Alicia Zegelien <aliciazegelien@Alicias-MacBook-Pro.local>
2013-10-17 19:17:38 -0400

    I commented out my if and else statement and instead I used a ternary operator. This activity of the bulletins was perfect for the ternary because it take 3 simple steps.

--- Zegelien_Alicia_Conditionals/Conditional_Personal/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Personal/js/script.js
@@ -1,10 +1,14 @@
 //Alicia Zegelien 10/17/2013 Conditional_Personal
 // The variable I am going to use for this assignment is people
 var people = prompt("Number of people per service?");
+var bulletins;
 // If number of people is greater than 75 make 100 bulletins. Otherwise, make 75.
-if(people > 75){
-	console.log("Make 100 bulletins!");
-}else{
-	console.log("Make 75 bulletins!");
-}
+//if(people > 75){
+//	console.log("Make 100 bulletins!");
+//}else{
+//	console.log("Make 75 bulletins!");
+//}
 //above is the if statement and the else statement
+//below is a ternary statement for this same Condition.
+bulletins = (people > 75) ? console.log("Make 100 bulletins!") : console.log("Make 75 bulletins!");
+console.log(bulletins);
