e33ce3a69d604e2344d414e402b316048ad768cc
Alicia Zegelien <aliciazegelien@Alicias-MacBook-Pro.local>
2013-10-17 18:51:17 -0400

    Changed the title of my html to conditional_personal and changed the var name from bulletins to people.

--- Zegelien_Alicia_Conditionals/Conditional_Personal/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Personal/js/script.js
@@ -1,3 +1,4 @@
 //Alicia Zegelien 10/17/2013 Conditional_Personal
 // The variable I am going to use for this assignment is people
-var bulletins = prompt("Number of people per service?");
+var people = prompt("Number of people per service?");
+// If num
