00d99ea3e7c4b9b153358c2320b2742609d9c790
Alicia Zegelien <aliciazegelien@Alicias-MacBook-Pro.local>
2013-10-17 19:03:15 -0400

    Added my if , else statement. If greater than 75 make x bulletins if not make y bulletins

--- Zegelien_Alicia_Conditionals/Conditional_Personal/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Personal/js/script.js
@@ -2,3 +2,9 @@
 // The variable I am going to use for this assignment is people
 var people = prompt("Number of people per service?");
 // If number of people is greater than 75 make 100 bulletins. Otherwise, make 75.
+if(people > 75){
+	console.log("Make 100 bulletins!")
+}else{
+	console.log("Make 75 bulletins!")
+}
+//above is the if statement and the else statement
