1a2e3e7e8db9fb91aa362f88afb9a3adc96c37e6
Alicia Zegelien <aliciazegelien@Alicias-MacBook-Pro.local>
2013-10-17 21:17:44 -0400

    I added a variable to figure out the secret code

--- Zegelien_Alicia_Conditionals/Conditional_Wacky/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Wacky/js/script.js
@@ -1,15 +1,3 @@
-//Conditdional logic - else if
-
-var kidHeight = 50;
-var minHeight = 48;
-var wParentHeight =45;
-
-//if the child is old enough, print to the console "you can ride!"
-//if the kid is over 48 inches is height
-if(kidHeight > minHeight){
-	console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
-	console.log("You can ride, but only with a parent present!");
-}else{
-	console.log("Sorry kid grow some first!");
-}
+//Alicia Zegelien 10/17/2013 conditional_wacky
+//guess the secret code word
+var secretCodeWord = tootles
