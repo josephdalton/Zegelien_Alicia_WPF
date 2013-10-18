3255c8d8f347e882e42b7a25b1a91c810190fbe7
Alicia Zegelien <aliciazegelien@Alicias-MacBook-Pro.local>
2013-10-17 20:21:55 -0400

    added comment on how I am going to work my condition.

--- Zegelien_Alicia_Conditionals/Conditional_Industrial/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Industrial/js/script.js
@@ -1,3 +1,5 @@
 //Alicia Zegelien 10/17/13 Conditional_Industrial
 //ratios for 1 year old classrooms
 var oneYearRatio = prompt("How many one year olds in your classroom?");
+
+//if you have 6 or less kids you can be by yourself, if you have less than or equal to 12 you can have two people. otherwise, send to another room what you are over.
