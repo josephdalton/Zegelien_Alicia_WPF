d32910ef5318c227201ad602ecb8db71fd0f8585
Alicia Zegelien <aliciazegelien@Alicias-MacBook-Pro.local>
2013-10-17 20:30:08 -0400

    added the else statement that anything over 12 needs to be moved to another classroom.

--- Zegelien_Alicia_Conditionals/Conditional_Industrial/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Industrial/js/script.js
@@ -4,7 +4,9 @@ var oneYearRatio = prompt("How many one year olds in your classroom?");
 
 //if you have 6 or less kids you can be by yourself, if you have less than or equal to 12 you can have two people. otherwise, send to another room what you are over.
 if(oneYearRatio <= 6){
-	console.log("You can be in a room by yourself.");
-}else if(oneYearRatio > 6 && oneYearRatio < 12){
-	console.log("You can be in the room with two people.");
+	console.log("You can be in a room by yourself.");  //if statement
+}else if(oneYearRatio > 6 && oneYearRatio <= 12){
+	console.log("You can be in the room with two people."); //else if statement
+}else{
+	console.log("Send what you are over to another classroom."); //else statement
 }
