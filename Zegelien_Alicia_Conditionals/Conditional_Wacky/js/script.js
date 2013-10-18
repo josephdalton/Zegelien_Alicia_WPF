058ce4f714c7c9c68406008fbf1b52994bca782f
Alicia Zegelien <aliciazegelien@Alicias-MacBook-Pro.local>
2013-10-17 21:26:37 -0400

    Added if statement if you know the secret code  congratulations!

--- Zegelien_Alicia_Conditionals/Conditional_Wacky/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Wacky/js/script.js
@@ -2,3 +2,6 @@
 //guess the secret code word
 var secretCodeWord = tootles
 var enteredSecretCode = prompt("What is the secret code word?");
+if(enteredSecretCode === secretCodeWord){
+	console.log("Congratulations you know the secret code word!!!");
+}
