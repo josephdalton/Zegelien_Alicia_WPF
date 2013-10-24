6f4df1d1e11b586116ab7d3b11194a3fb7f57ea6
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-24 16:56:00 -0400

    example variable scope

--- Git repo./Associating js 2 /js/script.js
+++ Git repo./Associating js 2 /js/script.js
@@ -4,17 +4,17 @@
 //(for parameters)
 //functiom has a functionsName
 
-function outputMsg(){
-	console.log("Hello World");
-}
+//function outputMsg(){
+//	console.log("Hello World");
+//}
+var width = 5;
+
 
 function calcArea(){
 	var width = 20;
 	var height = 30;
 	var area = width * height;
-	console.log(area);
+	//console.log(area);
 }
-
-calcArea();
-calcArea();
+console.log(width);
 calcArea();
