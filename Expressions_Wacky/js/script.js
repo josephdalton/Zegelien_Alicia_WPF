a6a5f3bf437a42e1dfec8560a89b96ff6100ff8c
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-24 17:46:50 -0400

    MOVED TO NEW FOLDER

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,4 @@
+//Alicia Zegelien 10-9-13 Expresson_Wacky
+var trickOrTreaters = [45,70,52];
+var totalOfTrickOrTreaters = trickOrTreaters[0] + trickOrTreaters[1] + trickOrTreaters[2];
+console.log(totalOfTrickOrTreaters);
