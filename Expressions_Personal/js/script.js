869115ec357243459a5b7ffbebb00312ee1ea7f1
Alicia Zegelien <aliciazegelien@unknowna8bbcf0e8918.att.net>
2013-10-24 17:45:52 -0400

    MOVED TO NEW FOLDER

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,6 @@
+//Alicia Zegelien 10/9/13 Expressions_Personal
+var kids = prompt("Number of kids in the room:");
+var diaperChange = prompt("Number of times the kids are changed daily");
+var diapers = kids * diaperChange;
+//console.log(diapers);
+alert("The number of diapers I change a day = " + diapers);
