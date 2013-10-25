a282ef6f7cd84f4e2c603bc5a062bedc1aa8ac2c
josephdalton <jdalton@fullsail.com>
2013-10-25 13:11:59 -0400

    JMD-comments
    This code doesn't work.

--- Zegelien_Alicia_Conditionals/Conditional_Wacky/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Wacky/js/script.js
@@ -1,6 +1,6 @@
 //Alicia Zegelien 10/17/2013 conditional_wacky
 //guess the secret code word
-var secretCodeWord = tootles
+var secretCodeWord = 'tootles'; //incorrect code (has been corrected)
 var enteredSecretCode = prompt("What is the secret code word?");
 if(enteredSecretCode === secretCodeWord){        //if statement
 	console.log("Congratulations you know the secret code word!!!");
