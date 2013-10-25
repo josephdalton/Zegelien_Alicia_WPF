9f58bd6213e8476b66d6c06b09a3a965e05a4494
Alicia Zegelien <aliciazegelien@Alicias-MacBook-Pro.local>
2013-10-24 23:22:24 -0400

    Added first part of named function with parameters

--- Function_Personal/js/script.js
+++ Function_Personal/js/script.js
@@ -1,25 +1,4 @@
-console.log("========== Loops =========")
+//Alicia Zegelien 10/24/2013 Functions_Personal
 
-var b = 10; //setups the index
+var gross = monthlyWage(8,40,4);
 
-while(b > 0){ //checks the condition
-	console.log(b + " kegs on the wall");
-	b--; //increments or decrements the index
-}
-
-console.log("========= Do While Loops =========");
-var c = 10;
-do{
-	console.log(c + " kegs on the wall"); // this first
-	c--;
-}
-while(c > 0); // this second
-
-
-
-console.log("============= For Loops ==========");
-
-
-for(var I = 10; I > 0; I--){                 // shorthand version. this is while and do while
-	console.log(I + " kegs on the wall");
-}
