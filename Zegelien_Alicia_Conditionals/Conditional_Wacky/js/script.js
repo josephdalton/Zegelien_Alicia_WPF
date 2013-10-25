79ef072cfc5919ae25e8def96643108bc4643938
josephdalton <jdalton@fullsail.com>
2013-10-25 13:07:44 -0400

    JMD

--- Zegelien_Alicia_Conditionals/Conditional_Wacky/js/script.js
+++ Zegelien_Alicia_Conditionals/Conditional_Wacky/js/script.js
@@ -1,6 +1,6 @@
 //Alicia Zegelien 10/17/2013 conditional_wacky
 //guess the secret code word
-var secretCodeWord = tootles
+var secretCodeWord = 'tootles'; //Missing quotes and semicolon.
 var enteredSecretCode = prompt("What is the secret code word?");
 if(enteredSecretCode === secretCodeWord){        //if statement
 	console.log("Congratulations you know the secret code word!!!");
